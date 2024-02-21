8213f2955d9bf9271ad8f63d49468c6318ee2e0d
Xorlent <94985297+Xorlent@users.noreply.github.com>
2024-02-20 20:46:56 -0800

    Update Get-AllGithubReleases.ps1

--- tools/Get-AllGithubReleases.ps1
+++ tools/Get-AllGithubReleases.ps1
@@ -18,29 +18,32 @@ if (-not(Test-Path -Path $WorkingDir -PathType Container)){$null = New-Item -Pat
 # Force TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+# Get the list of releases, 100 items at a time (max API limit)
 $Response = Invoke-WebRequest -Method GET -Uri $ReleasesURL -Headers @{ 'Accept' = 'application/vnd.github.json'; 'X-Github-Api-Version' = '2022-11-28'} -UseBasicParsing
 
-if($Response.Statuscode -ne 200){exit -1} # Invalid result code from API
+if($Response.Statuscode -ne 200){exit -1} # Quit if we get an invalid result code from API
 $i = 1
 while($Response.Headers.link.Contains('rel="next"')){ # Walk through every page of responses
     $ReleaseList = ConvertFrom-Json $response.Content
     $i++
     foreach($record in $ReleaseList){
         foreach($asset in $record.assets){
-            if($asset.browser_download_url.Contains('win-x64.zip') -or $asset.browser_download_url.Contains('win-x86.zip')){
-                $FetchList.Add($asset.browser_download_url)
+            if($asset.browser_download_url.Contains('win-x64.zip') -or $asset.browser_download_url.Contains('win-x86.zip')){ # Specific to test repo.  Considering ways to make this more flexible
+                $FetchList.Add($asset.browser_download_url) # Add the maching download URL to our list of files we need to download
                 }
             }
         }
-    $ReleasesURLNext = $ReleasesURL + '&page=' + $i
+    $ReleasesURLNext = $ReleasesURL + '&page=' + $i # Increment the API call page number
+    # Get the next list of up to 100 items
     $Response = Invoke-WebRequest -Method GET -Uri $ReleasesURLNext -Headers @{ 'Accept' = 'application/vnd.github.json'; 'X-Github-Api-Version' = '2022-11-28'} -UseBasicParsing
 }
+# Only one page in API call results or we've now reached the last page of results
 $ReleaseList = ConvertFrom-Json $response.Content
 
 foreach($record in $ReleaseList){
     foreach($asset in $record.assets){
         if($asset.browser_download_url.Contains('win-x64.zip') -or $asset.browser_download_url.Contains('win-x86.zip')){
-            $FetchList.Add($asset.browser_download_url)
+            $FetchList.Add($asset.browser_download_url) # Add the maching download URL to our list of files we need to download
             }
         }
     }
@@ -48,6 +51,7 @@ foreach($record in $ReleaseList){
 $i = 1
 foreach($URL in $FetchList){
     $FileName = $WorkingDir + $i + '.zip'
+    # Download the file.  Assumes we're grabbing .zip releases only, not packaged installers.
     Invoke-Webrequest -URI $URL -OutFile $FileName
     $FileName
 
@@ -57,6 +61,7 @@ foreach($URL in $FetchList){
 
     $zipFile = [IO.Compression.ZipFile]::OpenRead( $FileName )
     try {
+        # Check the archive volume for the file in question.  If found, extract it into a subfolder with the same name as the zip file.
         if( $foundFile = $zipFile.Entries.Where({ $_.Name -eq $exe }, 'First') ) {
             $destinationFile = Join-Path $destinationDir $foundFile.Name
             [IO.Compression.ZipFileExtensions]::ExtractToFile( $foundFile[ 0 ], $destinationFile )
@@ -70,9 +75,12 @@ foreach($URL in $FetchList){
             $zipFile.Dispose()
         }
     }
+    # We're done with this downloaded file.  Delete it.
     Remove-Item -Path $FileName
     $i++
 }
 $splitExe = $exe.Split(".")
 $csvOutput = "$PWD.Path\hashes\$splitExe[0].csv"
+
+# Get the list of all maching files in our working directory (recursively), piping to a formatted table and sending that table to a ready-to-use CSV file.
 Get-ChildItem $WorkingDir -Recurse -filter $exe | Select-Object -Property @{Name="MD5";expression={(Get-FileHash $_.FullName -Algorithm MD5).hash}},@{Name="SHA256";expression={(Get-FileHash $_.FullName).hash}},@{Name="SIZE";expression={$_.Length}} | export-csv $csvOutput
